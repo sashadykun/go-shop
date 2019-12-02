3181d9bb7c3d535d16d5dca9994ed51af0fb82ee
SD <sashaluckyman@gmail.com>
2019-12-01 19:31:48 -0800

    add shop page

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import SHOP_DATA from './shop.data.js';
+
+
+class ShopPage extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collections: SHOP_DATA
+        }
+    }
+
+    render() {
+        return (
+            <div>ShopPage</div>
+        )
+    }
+}
+
+export default ShopPage;
