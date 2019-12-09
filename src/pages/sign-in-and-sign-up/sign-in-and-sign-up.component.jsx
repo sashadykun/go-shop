d4ce83ce2414de0f5304bfec3f2e06a2197af460
SD <sashaluckyman@gmail.com>
2019-12-08 22:11:05 -0800

    add sign in form

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
 import './sign-in-and-sign-up.styles.scss';
 
 
 
 const SignInAndSignUpPage = () => (
-    <div>
-        <h1>SignInAndSignUp page</h1>
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
     </div>
 
 );
