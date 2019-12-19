da4f1fde34799327e194718712358909a5acdade
SD <sashaluckyman@gmail.com>
2019-12-18 23:06:09 -0800

    add handleChange and handleSubmit methods

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component';
 import { auth,createUserProfileDocument } from '../../firebase/firebase.utils';
@@ -15,6 +15,37 @@ class SignUp extends Component {
             password: '',
             confirmPassword: ''
         }
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault();
+        const { displayName, email, password, confirmPassword } = this.state;
+
+        if (password !== confirmPassword) {
+            alert("passwords don't match");
+            return
+        }
+
+        try {
+            const { user } = await auth.createUserWithEmailAndPassword(email, password);
+            await  createUserProfileDocument(user, { displayName })
+
+            this.setState({
+                displayName: '',
+                email: '',
+                password: '',
+                confirmPassword: ''
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value })
+
     }
 
     render() {
