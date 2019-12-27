eada111b33f7409568aedc8ef5b7cb88235eb62b
SD <sashaluckyman@gmail.com>
2019-12-26 21:57:07 -0800

    add redux for toggle dropdown by clicking on it

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import { ReactComponent as Logo} from '../../assets/goshop-logo.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser}) => (
+const Header = ({ currentUser, hidden }) => (
     <div className='header'>
         <Link className='logo-container' to='/'>
             <Logo className='logo' />
@@ -35,12 +35,16 @@ const Header = ({ currentUser}) => (
             }
             <CartIcon />
         </div>
-        <CartDropdown />
+        {
+            hidden ? null :
+            <CartDropdown />
+        } 
     </div>
 );
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 
